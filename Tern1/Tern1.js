c010a2a509f3b56b139f9f53b6ab8e0058061b86
MISF0RTUNEE <dimakom2000@gmail.com>
2023-09-28 21:48:12 +0300

    123
    123

--- Tern1/Tern1.js
+++ Tern1/Tern1.js
@@ -1,4 +1,5 @@
 let a = Math.floor(Math.random() * 100);
+console.log(a);
 let b = a;
 let c = a;
 a = (a > 10 ? a : a * 2) > 5 ? (2 * a) + 1 : (a < 3 ? 1 : 2 * (a - 2)) > 4 ? 5 : (a % 2 == 0 ? 6 : 7);
@@ -42,30 +43,30 @@ console.log(b);
 
 
 //switch
-switch(c){
+switch(true){
     case c > 10:
         break;
     default:
         c = c * 2;
 }
-switch(c){
+switch(true){
     case c > 5:
         c = (2 * c) + 1;
         break;
     default:
-        switch(c){
+        switch(true){
             case c < 3:
                 c = 1;
                 break;
             default:
                 c = 2 * (c - 2);
         }
-        switch(c){
+        switch(true){
             case c > 4:
                 c = 5;
                 break;
             default:
-                switch(c){
+                switch(true){
                     case c % 2 == 0:
                         c = 6;
                         break;
