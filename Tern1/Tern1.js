5844f23cacc4695958a17a6b74d0c8ca4fd9bced
MISF0RTUNEE <dimakom2000@gmail.com>
2023-09-28 22:45:59 +0300

    123
    123

--- Tern1/Tern1.js
+++ Tern1/Tern1.js
@@ -1,9 +1,9 @@
 let a = Math.floor(Math.random() * 100);
-console.log(a);
+console.log("Начальное значение: " + a);
 let b = a;
 let c = a;
 a = (a > 10 ? a : a * 2) > 5 ? (2 * a) + 1 : (a < 3 ? 1 : 2 * (a - 2)) > 4 ? 5 : (a % 2 == 0 ? 6 : 7);
-console.log(a);
+console.log("Тернарный оператор: " + a);
 // условие с условным (тернарным) оператором перевести в if...else И switch()
 // результат выводить в консоль, с пощью console.log()
 
@@ -21,14 +21,14 @@ if(b > 5){
     }
 }
 else{
-    if(b < 3){
-        b = 1;
-    }
-    else{
-        b = 2 * (b - 2)
-    }
     if(b > 4){
         b = 5;
+        if(b < 3){
+            b = 1;
+        }
+        else{
+            b = 2 * (b - 2)
+        }
     }
     else{
         if(b % 2 == 0){
@@ -40,7 +40,7 @@ else{
     }
 }
 
-console.log(b);
+console.log("if...else: " + b);
 
 
 //switch
@@ -55,17 +55,17 @@ switch(true){
                 c = c * 2;
         }
         break;
-    default:
-        switch(true){
-            case c < 3:
-                c = 1;
-                break;
-            default:
-                c = 2 * (c - 2);
-        }
+    default:       
         switch(true){
             case c > 4:
                 c = 5;
+                switch(true){
+                    case c < 3:
+                        c = 1;
+                        break;
+                    default:
+                        c = 2 * (c - 2);
+                }
                 break;
             default:
                 switch(true){
@@ -78,4 +78,4 @@ switch(true){
         }
 }
 
-console.log(c);
+console.log("Switch: " + c);
