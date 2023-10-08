1b2e2dcb1f683e1d59c9f1cd4806bdeeda5e68df
MISF0RTUNEE <dimakom2000@gmail.com>
2023-10-08 21:54:45 +0300

    123
    123

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,12 +1,18 @@
+let decided = false;
 
 document.querySelector('.buttons').onclick = (event) =>{
 
     if(event.target.className == "btn" ){
+        if(decided){
+            document.querySelector('.input').value = "";
+            decided = false;
+        }
         const key = event.target.textContent;
         document.querySelector('.input').value+= key;
     }
 
     if(event.target.className == "btn equals"){
         document.querySelector('.input').value += "=" + eval(document.querySelector('.input').value)
+        decided = true;
     }
 }
