8e710eb43f81de5b661107f0262a3465bcc33283
MISF0RTUNEE <dimakom2000@gmail.com>
2023-10-08 21:47:44 +0300

    123
    123

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,8 +1,12 @@
 
 document.querySelector('.buttons').onclick = (event) =>{
 
-    if(event.target.className == "btn" && event.target.className != "equals"){
+    if(event.target.className == "btn" ){
         const key = event.target.textContent;
         document.querySelector('.input').value+= key;
+    }
+
+    if(event.target.className == "btn equals"){
+        document.querySelector('.input').value += "=" + eval(document.querySelector('.input').value)
     }
 }
