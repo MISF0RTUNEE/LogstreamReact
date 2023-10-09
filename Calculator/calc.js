eaeffe691b4eb6793865b1a796e31617fbf9e397
MISF0RTUNEE <dimakom2000@gmail.com>
2023-10-09 19:10:35 +0300

    123
    123

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,7 +1,6 @@
 let decided = false;
 
 document.querySelector('.buttons').onclick = (event) =>{
-
     if(event.target.className == "btn" ){
         if(decided){
             document.querySelector('.input').value = "";
@@ -27,3 +26,21 @@ document.querySelector('.buttons').onclick = (event) =>{
         document.querySelector('.input').value = document.querySelector('.input').value.slice(0,-1);
     }
 }
+const keys = ['1', '2', '3', '4', '5','6', '7', '8', '9', '0', '(', ')', '+', '-','*','/','.']
+document.addEventListener('keydown', (event) => {
+    const key = event.key;
+    if(keys.includes(key)){
+        if(decided){
+            document.querySelector('.input').value = "";
+            decided = false;
+        }
+        document.querySelector('.input').value+= key;
+    }
+    if(key == "Enter"){
+        document.querySelector('.input').value += "=" + eval(document.querySelector('.input').value)
+        decided = true;
+        let p = document.createElement("p");
+        p.textContent = document.querySelector('.input').value
+        document.querySelector('.history').append(p);
+    }
+})
